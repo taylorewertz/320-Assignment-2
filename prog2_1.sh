5f492870b804a698267b07c4ba5cbf19c6a2eda9
Taylor Ewertz <t_ewertz@yahoo.com>
2016-09-19 16:54:31 -0700

    added command line functionality

--- prog2_1.sh
+++ prog2_1.sh
@@ -2,10 +2,10 @@
 
 echo "Assignment # 2-1, Taylor Ewertz, t_ewertz@yahoo.com"
 
-first=`grep 100 Grades | cut -d',' -f1`
-last=`grep 100 Grades | cut -d',' -f2`
-uname=`grep $first Logins | cut -d',' -f2`
-pword=`grep $first Logins | cut -d',' -f3`
+first=`grep 100 $1 | cut -d',' -f1`
+last=`grep 100 $1 | cut -d',' -f2`
+uname=`grep $first $2 | cut -d',' -f2`
+pword=`grep $first $2 | cut -d',' -f3`
 
 echo $first $last
 echo $uname
