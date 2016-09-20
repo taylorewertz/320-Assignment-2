2f5b73c570067138b7ca1a0e89d5918dc005e08a
Taylor Ewertz <t_ewertz@yahoo.com>
2016-09-20 15:18:27 -0700

    suppressed warning messages and got correct output

--- prog2_3.sh
+++ prog2_3.sh
@@ -4,21 +4,21 @@ echo "Assignment #2-3, Taylor Ewertz, t_ewertz@yahoo.com"
 
 for aone in `grep -lr "%.3f" $1`
 do
-	gcc  $aone -lm
-	echo `basename $aone`.c "Assignment #1"
+	gcc -w  $aone -lm
+	echo `basename $aone` "Assignment #1"
 done
 for atwo in `grep -lr "%.4f" $2`
 do
-	gcc $atwo -lm
-	echo `basename $atwo`.c "Assignment #2"
+	gcc -w $atwo -lm
+	echo `basename $atwo` "Assignment #2"
 done
 for athree in `grep -lr ".5f" $3`
 do
-	gcc $athree -lm
-	echo `basename $athree` .c "Assignment #3"
+	gcc -w $athree -lm
+	echo `basename $athree` "Assignment #3"
 done
 for afour in `grep -lr "main" $4`
 do
-	gcc $afour -lm
-	echo `basename $afour` .c "Assignment #4"
+	gcc -w $afour -lm
+	echo `basename $afour`  "Assignment #4"
 done	
