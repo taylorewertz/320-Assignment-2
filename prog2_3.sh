f81a906310fcaf2884df3ead56cde6d74224beb0
Taylor Ewertz <t_ewertz@yahoo.com>
2016-09-20 16:15:46 -0700

    Made print out correctly no matter what the order is

--- prog2_3.sh
+++ prog2_3.sh
@@ -1,23 +1,24 @@
+
 #!/bin/bash
 
 echo "Assignment #2-3, Taylor Ewertz, t_ewertz@yahoo.com"
 
-for aone in `grep -lr "%.3f" $1`
+for aone in `grep -lr "%.3f" $1 $2 $3 $4`
 do
 	gcc -w  $aone -lm
 	echo `basename $aone` "Assignment #1"
 done
-for atwo in `grep -lr "%.4f" $2`
+for atwo in `grep -lr "%.4f" $1 $2 $3 $4`
 do
 	gcc -w $atwo -lm
 	echo `basename $atwo` "Assignment #2"
 done
-for athree in `grep -lr ".5f" $3`
+for athree in `grep -lr ".5f" $1 $2 $3 $4`
 do
 	gcc -w $athree -lm
 	echo `basename $athree` "Assignment #3"
 done
-for afour in `grep -lr "main" $4`
+for afour in `grep -lr "2.f" $1 $2 $3 $4`
 do
 	gcc -w $afour -lm
 	echo `basename $afour`  "Assignment #4"
